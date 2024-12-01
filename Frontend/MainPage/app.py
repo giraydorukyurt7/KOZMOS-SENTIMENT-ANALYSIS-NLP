bd7e51f8c7b1fc02ef16c48077a020ad2eab95ea
giraydorukyurt7 <giraysoftwareengineer3.14@hotmail.com>
2024-12-01 12:43:02 +0300

    Frontend 8

---
+++ Frontend/MainPage/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# load model
+with open('log_final.pkl', 'rb') as f:
+    log_model = pickle.load(f)
+
+@app.route('/')
+def main_page():
+    return render_template('mainpage.html')
+
+@app.route('/predict_rf', methods=['POST'])
+def predict_log():
+    user_input = request.form['user_input']
+    predict_log = log_model.predict([user_input])
+    return jsonify({'prediction': prediction_log[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)    
